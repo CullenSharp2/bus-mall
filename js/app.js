599545f42c81bdb00862d5d618c9efb7887e1fa7
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-22 15:03:51 -0700

    Instatiate objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function CatalogItem (name, path, views) {
+    this.name = name;
+    this.path = path;
+    this.views = views;
+}
+
+const wineGlass = new CatalogItem('wine-glass', 'imgs/wine-glass.jpg', 0);
+const waterCan = new CatalogItem('water-can', 'imgs/water-can.jpg', 0);
+const usb = new CatalogItem('usb', 'imgs/ugb.gif', 0);
+const unicorn = new CatalogItem('unicorn', 'imgs/unicorn.jpg', 0);
+const tauntaun = new CatalogItem('tauntaun', 'imgs/tauntaun.jpg', 0);
+const sweep = new CatalogItem('sweep', 'imgs/sweep.jpg', 0);
+const shark = new CatalogItem('shark', 'imgs/shark.jpg', 0);
+const scissors = new CatalogItem('scissors', 'imgs/scissors.jpg', 0);
+const petSweep = new CatalogItem ('pet-sweep', 'imgs/pet-sweep.jpg', 0);
+const pen = new CatalogItem('pen', 'imgs/pen.jpg', 0);
+const dragon = new CatalogItem('dragon', 'imgs/dragon.jpg', 0);
+const dogDuck = new CatalogItem('dog-duck', 'imgs/dog-duck.jpg', 0);
+const cthulhu = new CatalogItem('cthulhu', 'imgs/cthulhu.jpg', 0);
+const chair = new CatalogItem('chair', 'imgs/chair.jpg', 0);
+const bubblegum = new CatalogItem('bubblegum', 'imgs/bubblegum.jpg', 0);
+const breakfast = new CatalogItem('breakfast', 'imgs/breakfast.jpg', 0);
+const boots = new CatalogItem('boots', 'imgs/boots.jpg', 0);
+const bathroom = new CatalogItem('bathroom', 'imgs/bathroom.jpg', 0);
+const banana = new CatalogItem('banana', 'imgs/banana.jpg', 0);
+const bag = new CatalogItem ('bag', 'imgs/bag.jpg', 0);
