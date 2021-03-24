8bff5eb49a485dcd4d6e64cba12f6022019e040a
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-24 15:04:42 -0700

    Implement local storage

--- js/app.js
+++ js/app.js
@@ -104,7 +104,7 @@ function voteHandler(event) {
         }
         render(Item.items);
     } else {
-        console.log(currentRound);
+        setStoredItems();
         for (let i of document.getElementsByTagName('img')) {
             i.removeEventListener('click', voteHandler);
         }
@@ -200,4 +200,23 @@ function render(items) {
     }
 }
 
+function setStoredItems() {
+    let stringifiedItems = JSON.stringify(Item.items);
+    localStorage.setItem('item', stringifiedItems);
+}
+
+function getStoredItems() {
+    let storedItems = localStorage.getItem('item');
+
+    if (storedItems !== null) {
+        let parsedItems = JSON.parse(storedItems);
+        console.log(parsedItems.length)
+        for (let i in parsedItems) {
+            Item.items[i].views += parsedItems[i].views;
+            Item.items[i].likes += parsedItems[i].likes;
+            // new Item(item.name, item.path);
+        }
+    }
+}
+getStoredItems();
 render(Item.items);
